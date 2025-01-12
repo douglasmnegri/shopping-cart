bc192866acf2705b7174aa4d29a166d7d45e338b
douglasmnegri <douglasmnegri@gmail.com>
2025-01-12 16:49:01 -0300

    feat: structure the directories of the project

---
+++ src/Button/Button.jsx
@@ -0,0 +1,12 @@
+// Button.jsx
+import classes from "./button.module.css";
+
+function Button() {
+  return (
+    <div>
+      <button className={classes.mainBtn}>Default</button>
+    </div>
+  );
+}
+
+export default Button;
