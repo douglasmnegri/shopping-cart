594dd20deccca1259d6657e0009f690de8f505a5
douglasmnegri <douglasmnegri@gmail.com>
2025-01-13 21:43:20 -0300

    feat: add smaller banners

--- src/Home/Body/Body.jsx
+++ src/Home/Body/Body.jsx
@@ -4,6 +4,8 @@ import {
   faCartShopping,
   faTruck,
 } from "@fortawesome/free-solid-svg-icons";
+import scarletViolet from "../../assets/sv_banner.png";
+import horizonsSeries from "../../assets/hs_banner.png";
 
 import banner from "../../assets/tcg.png";
 import classes from "./body.module.css";
@@ -15,9 +17,30 @@ function Body() {
         <img src={banner} alt="pokemon banner tcg" className={classes.banner} />
       </div>
       <div className={classes.ruler}>
-        <IconContent icon={faCartShopping} content="Special Prices on Bundles" />
+        <IconContent
+          icon={faCartShopping}
+          content="Special Prices on Bundles"
+        />
         <IconContent icon={faBox} content="30-Day Exchange Policy" />
         <IconContent icon={faTruck} content="Same-day Shipping" />
+      </div>
+      <div className={classes.rectBannerContainer}>
+        <div className={classes.rectBannerBox}>
+          <img
+            src={scarletViolet}
+            alt="scarlet and violet banner"
+            className={classes.rectBanner}
+          />
+          <div className={classes.content}>Scarlet & Violet</div>{" "}
+          
+        </div>
+        <div className={classes.rectBannerBox}>
+          <img
+            src={horizonsSeries}
+            alt="horizons series banner"
+            className={classes.rectBanner}
+          />
+        </div>
       </div>
     </>
   );
