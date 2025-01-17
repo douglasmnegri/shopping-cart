5bb5ce3a22cee65f80f61c655487b8c8dcd5de2c
douglasmnegri <douglasmnegri@gmail.com>
2025-01-17 20:37:23 -0300

    feat: create router

---
+++ src/Shop/Shop.jsx
@@ -0,0 +1,13 @@
+import Header from "../Home/Header/Header";
+import Footer from "../Home/Footer/Footer";
+
+function Shop() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Shop;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Shop from "./Shop/Shop";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
