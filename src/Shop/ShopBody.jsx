e72268eaea1250fc0345fd3086fec8c6f13d066d
douglasmnegri <douglasmnegri@gmail.com>
2025-01-18 16:19:03 -0300

    feat: make API request for multiple cards

--- src/Shop/ShopBody.jsx
+++ src/Shop/ShopBody.jsx
@@ -3,45 +3,55 @@ import Cards from "./Cards/Cards";
 
 const apiKey = import.meta.env.VITE_POKEMON_TCG_API_KEY;
 
-const pokeCards = { card: "bp-6", price: 20.35 };
 function ShopBody() {
-  const [id, setID] = useState(["bp-1", "bp-2", "bp-3"]);
-  const [cards, setCards] = useState(null);
-  const url = `https://api.pokemontcg.io/v2/cards/${pokeCards.card}`;
-
+  const [cards, setCards] = useState([]);
+  const url = `https://api.pokemontcg.io/v2/cards/`;
   useEffect(() => {
+    const id = ["xy0-1", "xy2-1", "xy4-1"];
     const fetchCards = async () => {
       try {
-        const response = await fetch(url, {
-          headers: {
-            "X-Api-Key": apiKey,
-          },
-        });
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        setCards(json);
-        console.log(json);
+        const fetchedCards = await Promise.all(
+          id.map(async (cardId) => {
+            const response = await fetch(url + cardId, {
+              headers: {
+                "X-Api-Key": apiKey,
+              },
+            });
+            if (!response.ok) {
+              throw new Error(`Failed to fetch card with ID: ${cardId}`);
+            }
+            const json = await response.json();
+            console.log(json);
+            
+            return json.data;
+          })
+        );
+        setCards(fetchedCards);
       } catch (error) {
         console.error(error.message);
       }
     };
+
     fetchCards();
   }, []);
 
   return (
     <div>
       <h1>Test Card</h1>
-      {cards ? (
-        <Cards
-          img={cards.data.images.small}
-          name={cards.data.name}
-          // price={pokeCards.price}
-          artist={cards.data.artist}
-        />
+      {cards.length > 0 ? (
+        <div>
+          {cards.map((card) => (
+            <Cards
+              key={card.id}
+              img={card.images.small}
+              name={card.name}
+              price={card.cardmarket.prices.averageSellPrice}
+              artist={card.artist}
+            />
+          ))}
+        </div>
       ) : (
-        <p>Loading</p>
+        <p>Loading...</p>
       )}
     </div>
   );
